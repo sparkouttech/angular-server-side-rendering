7055883ba65735af86314716c6dbc1084d8a2f58
Sivabharathy <gotocva@gmail.com>
2022-11-04 13:46:56 +0530

    port update

--- data.js
+++ data.js
@@ -3,16 +3,16 @@
 const DEFAULT_META_DATA = {
     "app_title": 'default app_title',
     "meta_title": 'default meta_title',
-    "meta_description":'default meta_description',
+    "meta_description":'Default meta description Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
     "meta_keywords":'default meta_keywords',
     "meta_author":'default meta_author',
     "og:locale":'default og:locale',
     "og:type":'default og:type',
     "og:title":'default og:title',
-    "og:description":'default og:description',
+    "og:description":'Default sample meta description Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
     "og:url":'default og:url',
     "og:site_name":'default og:site_name',
-    "og:image":'default og:image',
+    "og:image":'https://www.tibs.org.tw/images/default.jpg',
     "og:image:width":'default og:image:width',
     "og:image:height":'default og:image:height',
     "og:image:alt":'default og:image:alt',
@@ -21,7 +21,7 @@ const DEFAULT_META_DATA = {
     "twitter:site":'default twitter site',
     "twitter:title":'default twitter title',
     "twitter:description":'default twitter description',
-    "twitter:image":'default twitter image',
+    "twitter:image":'https://www.tibs.org.tw/images/default.jpg',
 };
 
 
@@ -29,7 +29,7 @@ const META_DATA = {
     "/" : {
         "app_title": 'sample app_title',
         "meta_title": 'sample meta_title',
-        "meta_description":'sample meta_description',
+        "meta_description":'Home sample meta description Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
         "meta_keywords":'sample meta_keywords',
         "meta_author":'sample meta_author',
         "og:locale":'sample og:locale',
@@ -52,16 +52,16 @@ const META_DATA = {
     "/about-us" : {
         "app_title": 'about page app_title',
         "meta_title": 'sample meta_title',
-        "meta_description":'sample meta_description',
+        "meta_description":'About sample meta description Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
         "meta_keywords":'sample meta_keywords',
         "meta_author":'sample meta_author',
         "og:locale":'sample og:locale',
         "og:type":'sample og:type',
         "og:title":'sample og:title',
-        "og:description":'sample og:description',
+        "og:description":'About sample meta description Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
         "og:url":'sample og:url',
         "og:site_name":'sample og:site_name',
-        "og:image":'sample og:image',
+        "og:image":'https://st2.depositphotos.com/3591429/10566/i/600/depositphotos_105666254-stock-photo-business-people-at-meeting-and.jpg',
         "og:image:width":'sample og:image:width',
         "og:image:height":'sample og:image:height',
         "og:image:alt":'sample og:image:alt',
@@ -70,7 +70,7 @@ const META_DATA = {
         "twitter:site":'sample twitter site',
         "twitter:title":'sample twitter title',
         "twitter:description":'sample twitter description',
-        "twitter:image":'sample twitter image',
+        "twitter:image":'https://st2.depositphotos.com/3591429/10566/i/600/depositphotos_105666254-stock-photo-business-people-at-meeting-and.jpg',
     }
 }
 
